53a848344921a94db2ee518c31d2f26e153f26da
Janick Marbot <janick.marbot@gmail.com>
2024-12-04 21:07:33 +0100

    add part two of day three

--- d3/d3.ps1
+++ d3/d3.ps1
@@ -1,3 +1,5 @@
+# Part I
 ((Select-String -path .\d3.input -Pattern "mul\((\d{1,3},\d{1,3})\)" -AllMatches | foreach { $_.matches } | foreach { $_.groups[1].Value }).Replace(",", "*") | Invoke-Expression | foreach { [int]$_ } | measure -sum).sum
 
-
+# Part II: tricky part was the (?s) single line modifier and the ungreedy/lazy modfier to the * quantifier
+(((Get-Content -raw .\d3.input) -replace "(?s)don't\(\).*?do\(\)" | Select-String -Pattern "mul\((\d{1,3},\d{1,3})\)" -AllMatches | foreach { $_.matches } | foreach { $_.groups[1].Value }).Replace(",", "*") | Invoke-Expression | foreach { [int]$_ } | measure -sum).sum
